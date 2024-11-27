6997d2fb2a120ff403e146a6e2871bdea7b721da
AndrewLau97 <andrewlau1997@hotmail.co.uk>
2024-11-27 17:04:21 +0000

    task 17 get/users/:username endpoint with tests

--- model/users.models.js
+++ model/users.models.js
@@ -1,8 +1,21 @@
-const db=require("../db/connection")
+const db = require("../db/connection");
 
-
-function fetchUsers(){
-return db.query(`SELECT * FROM users`).then(({rows})=>{return rows})
+function fetchUsers() {
+  return db.query(`SELECT * FROM users`).then(({ rows }) => {
+    return rows;
+  });
 }
 
-module.exports={fetchUsers}
+function fetchUserbyUsername(username) {
+  return db
+    .query(`SELECT * FROM users WHERE username = $1`, [username])
+    .then(({ rows }) => {
+        if(rows.length===0){
+            return Promise.reject({status:404,message:"Not Found"})
+        }else{
+            return rows[0];
+        }
+    });
+}
+
+module.exports = { fetchUsers, fetchUserbyUsername };
--- routes/users-router.js
+++ routes/users-router.js
@@ -1,7 +1,9 @@
-const { getUsers } = require("../controller/users.controllers");
+const { getUsers, getUserByUsername } = require("../controller/users.controllers");
 
 const usersRouter=require("express").Router();
 
 usersRouter.get("/", getUsers)
+
+usersRouter.get("/:username",getUserByUsername)
 
 module.exports=usersRouter
